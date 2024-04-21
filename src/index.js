117a0eaed0d5424190c954ab59f54681537ae224
therealmaduanusi <84400462+therealmaduanusi@users.noreply.github.com>
2024-04-21 20:33:12 +0100

    index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,4 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-ReactDOM.render(
-  <div className="container">
-    <h1>0</h1>
-    <button>+</button>
-  </div>,
-  document.getElementById("root")
-);
+import App from "./components/App";
+ReactDOM.render(<App />, document.getElementById("root"));
